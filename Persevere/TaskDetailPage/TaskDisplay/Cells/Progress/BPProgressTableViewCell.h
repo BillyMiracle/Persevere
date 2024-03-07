35e872daf9c9210858416be8bca821f50dabced9
BillyMiracle <510986792@qq.com>
2024-03-08 02:11:15 +0800

    feat: 创建完成进度cell

---
+++ Persevere/TaskDetailPage/TaskDisplay/Cells/Progress/BPProgressTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  BPProgressTableViewCell.h
+//  Persevere
+//
+//  Created by 张博添 on 2024/3/8.
+//
+
+#import "BPTaskDetailBaseTableViewCell.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BPProgressTableViewCell : BPTaskDetailBaseTableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Persevere/TaskDetailPage/TaskDisplay/Cells/Progress/BPProgressTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  BPProgressTableViewCell.m
+//  Persevere
+//
+//  Created by 张博添 on 2024/3/8.
+//
+
+#import "BPProgressTableViewCell.h"
+
+@implementation BPProgressTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
