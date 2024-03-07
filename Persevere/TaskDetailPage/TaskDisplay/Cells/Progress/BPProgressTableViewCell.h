36219a9bfb21aa2e04204b26f7c78fb0363f64e7
BillyMiracle <510986792@qq.com>
2024-03-08 02:25:18 +0800

    feat: 完成进度cell开发

--- Persevere/TaskDetailPage/TaskDisplay/Cells/Progress/BPProgressTableViewCell.h
+++ Persevere/TaskDetailPage/TaskDisplay/Cells/Progress/BPProgressTableViewCell.h
@@ -9,7 +9,22 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+@interface BPProgressTableViewCellModel : NSObject
+
+/// 打卡天数
+@property (nonatomic, assign) NSInteger punchedDayCount;
+/// 总天数
+@property (nonatomic, assign) NSInteger totalDayCount;
+
+- (instancetype)initWithPunchedDayCount:(NSInteger)punchedDayCount totalDayCount:(NSInteger)totalDayCount;
+
+@end
+
+@class TaskModel;
+
 @interface BPProgressTableViewCell : BPTaskDetailBaseTableViewCell
+
+- (void)bindModel:(BPProgressTableViewCellModel *)model;
 
 @end
 
--- Persevere/TaskDetailPage/TaskDisplay/Cells/Progress/BPProgressTableViewCell.m
+++ Persevere/TaskDetailPage/TaskDisplay/Cells/Progress/BPProgressTableViewCell.m
@@ -6,6 +6,28 @@
 //
 
 #import "BPProgressTableViewCell.h"
+#import "BPProgressLabel.h"
+#import "BPUIHelper.h"
+
+@implementation BPProgressTableViewCellModel
+
+- (instancetype)initWithPunchedDayCount:(NSInteger)punchedDayCount totalDayCount:(NSInteger)totalDayCount {
+    self = [super init];
+    if (self) {
+        self.punchedDayCount = punchedDayCount;
+        self.totalDayCount = totalDayCount;
+    }
+    return self;
+}
+
+@end
+
+@interface BPProgressTableViewCell()
+
+@property (nonatomic, strong) BPProgressLabel *progressLabel;
+
+@end
+
 
 @implementation BPProgressTableViewCell
 
@@ -18,6 +40,32 @@
     [super setSelected:selected animated:animated];
 
     // Configure the view for the selected state
+}
+
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        [self.bp_backgroundView addSubview:self.progressLabel];
+    }
+    return self;
+}
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    self.progressLabel.frame = CGRectMake(0, 0, self.bp_backgroundView.bp_width, self.bp_backgroundView.bp_height);
+}
+
+- (void)bindModel:(BPProgressTableViewCellModel *)model {
+    [self.progressLabel setProgressWithFinished:model.punchedDayCount andTotal:model.totalDayCount];
+}
+
+// MARK: Getters
+
+- (BPProgressLabel *)progressLabel {
+    if (!_progressLabel) {
+        _progressLabel = [[BPProgressLabel alloc] init];
+    }
+    return _progressLabel;
 }
 
 @end
