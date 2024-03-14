668903538800360c1d83c8830e18bf73c91b091a
zhangbotian <zhangbotian@xiaohongshu.com>
2024-03-14 14:37:23 +0800

    feat: 创建infotab视图文件

---
+++ Persevere/UI/InfoTabView/BPInfoTabView.h
@@ -0,0 +1,16 @@
+//
+//  BPInfoTabView.h
+//  Persevere
+//
+//  Created by zhangbotian on 2024/3/14.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BPInfoTabView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Persevere/UI/InfoTabView/BPInfoTabView.m
@@ -0,0 +1,20 @@
+//
+//  BPInfoTabView.m
+//  Persevere
+//
+//  Created by zhangbotian on 2024/3/14.
+//
+
+#import "BPInfoTabView.h"
+
+@interface BPInfoTabView ()
+
+
+
+@end
+
+@implementation BPInfoTabView
+
+
+
+@end
