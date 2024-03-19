f7d00695883e591094447d30dc93385c60c3d1e1
zhangbotian <zhangbotian@xiaohongshu.com>
2024-03-19 14:06:27 +0800

    feat: 继续开发AR页面

--- Persevere/AR/BPARImageRecognizeViewController.swift
+++ Persevere/AR/BPARImageRecognizeViewController.swift
@@ -9,15 +9,30 @@ import UIKit
 import ARKit
 
 class BPARImageRecognizeViewController: UIViewController {
+    // MARK: 声明属性
     /// 任务数组
     private var taskArray: [TaskModel]
     /// AR view
     private lazy var sceneView: ARSCNView! = {
         let navigationBarHeight = UIDevice.bp_navigationFullHeight()
         let sceneView = ARSCNView.init(frame: CGRect.init(x: 0, y: navigationBarHeight, width: self.view.bp_width, height: self.view.bp_height - navigationBarHeight))
+        sceneView.delegate = self
+        sceneView.isPlaying = false
         return sceneView
     }()
+    /// AR Session
+    private lazy var arSession: ARSession = {
+        let session = ARSession()
+        return session
+    }()
+    /// AR 配置
+    private lazy var arWordTrackingConfiguration: ARWorldTrackingConfiguration = {
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.automaticImageScaleEstimationEnabled = true
+        return configuration
+    }()
     
+    // MARK: 私有方法
     init(taskArray: [TaskModel]) {
         self.taskArray = taskArray
         super.init(nibName: nil, bundle: nil)
@@ -27,13 +42,65 @@ class BPARImageRecognizeViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: 生命周期方法
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setupDetectionImages()
         
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.setUpSceneView()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
+}
+
+// MARK: 私有方法
+
+extension BPARImageRecognizeViewController {
+    /// 设置检测图片
+    private func setupDetectionImages() {
+        let tasks = self.taskArray
+        var detectionImages = Set<ARReferenceImage>()
+        for task in tasks {
+            if let taskImageData = task.imageData {
+                let taskImage = UIImage(data: taskImageData)
+                let refImage = ARReferenceImage((taskImage?.cgImage)!, orientation: .up, physicalWidth: 0.3)
+                refImage.name = task.name
+                detectionImages.insert(refImage)
+            }
+        }
+        self.arWordTrackingConfiguration.detectionImages = detectionImages
+    }
+    
+    /// 设定AR Scene View
+    private func setUpSceneView() {
         
     }
 }
 
-extension BPARImageRecognizeViewController {
+// MARK: ARSCNViewDelegate
+
+extension BPARImageRecognizeViewController: ARSCNViewDelegate {
+    public func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        DispatchQueue.main.async {
+            
+        }
+    }
     
+    public func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        DispatchQueue.main.async {
+            
+        }
+    }
+    public func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        DispatchQueue.main.async {
+            
+        }
+    }
 }
