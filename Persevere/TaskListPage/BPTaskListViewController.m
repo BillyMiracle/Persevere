9280f64ba4f6c9c7b29672c1ea42714ee21dba2c
BillyMiracle <510986792@qq.com>
2024-04-06 13:00:16 +0800

    feat: 为任务列表页增加设置按钮

--- Persevere/TaskListPage/BPTaskListViewController.m
+++ Persevere/TaskListPage/BPTaskListViewController.m
@@ -9,11 +9,14 @@
 #import "BPTaskListView.h"
 #import "BPNavigationTitleView.h"
 #import "BPUIHelper.h"
+#import "BPSettingViewController.h"
 
 @interface BPTaskListViewController ()
 <BPNavigationTitleViewDelegate>
 @property (nonatomic, strong) BPNavigationTitleView *taskNavigationTitleView;
 @property (nonatomic, strong) BPTaskListView *taskListPageView;
+/// 设置按钮
+@property (nonatomic, strong) UIBarButtonItem *settingButton;
 
 @end
 
@@ -24,6 +27,7 @@
     self.view.backgroundColor = [UIColor whiteColor];
     
     self.navigationItem.titleView = self.taskNavigationTitleView;
+    self.navigationItem.rightBarButtonItem= self.settingButton;
     
     [self.view addSubview:self.taskListPageView];
 }
@@ -31,6 +35,18 @@
 // MARK: BPNavigationTitleViewDelegate
 - (void)navigationTitleViewTapped {
     [self.taskListPageView navigationTitleViewTapped];
+}
+
+// MARK: Button Actions
+
+- (void)pressSortButton {
+    
+}
+
+- (void)pressSettingButton {
+    BPSettingViewController *settingPage = [[BPSettingViewController alloc] init];
+    settingPage.hidesBottomBarWhenPushed = YES;
+    [self.navigationController pushViewController:settingPage animated:YES];
 }
 
 // MARK: Getters
@@ -48,6 +64,14 @@
         _taskListPageView = [[BPTaskListView alloc] initWithFrame:CGRectMake(0, [UIDevice bp_navigationFullHeight], self.bp_width, self.bp_height - [UIDevice bp_navigationFullHeight] - [UIDevice bp_tabBarFullHeight])];
     }
     return _taskListPageView;
+}
+
+- (UIBarButtonItem *)settingButton {
+    if (!_settingButton) {
+        _settingButton = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"NavSetting"] style:UIBarButtonItemStylePlain target:self action:@selector(pressSettingButton)];
+        _settingButton.tintColor = [UIColor whiteColor];
+    }
+    return _settingButton;
 }
 
 @end
